e4c4779671a1b8cac26c1e6875c8e1b0a039b7a3
Salahs03 <salahsiam03@gmail.com>
2025-09-23 21:03:49 +0100

    Add traffic light simulator

---
+++ Function practise/traffic_light.py
@@ -0,0 +1,11 @@
+# Decide action based on traffic light colour
+light = "green"
+
+if light == "red":
+    print("Stop!")
+elif light == "yellow":
+    print("Get ready to stop.")
+elif light == "green":
+    print("Go!")
+else:
+    print("Invalid colour.")
