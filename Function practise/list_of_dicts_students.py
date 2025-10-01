5f99d3a4fcc2e250c0b89ea1d016a6227cded8cc
Salahs03 <salahsiam03@gmail.com>
2025-10-01 13:45:59 +0100

    Basic example of lists and dictionaries

---
+++ Function practise/list_of_dicts_students.py
@@ -0,0 +1,13 @@
+# A list containing multiple dictionaries
+students = [
+    {"name": "Alice", "age": 10, "grade": "A"},
+    {"name": "Bob", "age": 11, "grade": "B"},
+    {"name": "Charlie", "age": 10, "grade": "A"}
+]
+
+# Looping through the list of dictionaries
+for student in students:
+    print(student["name"], "is", student["age"], "years old and got grade", student["grade"])
+
+# Accessing a specific value
+print("Second student’s name:", students[1]["name"])
