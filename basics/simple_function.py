1558ef5669fe114939642d9555e51f19c21f110e
Salahs03 <salahsiam03@gmail.com>
2025-10-15 12:59:48 +0100

    Add files into basics

---
+++ basics/simple_function.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    """
+    Return the sum of a and b.
+    Simple example function to practice Git.
+    """
+    return a + b
+
+if __name__ == "__main__":
+    x = 2
+    y = 3
+    print(f"{x} + {y} =", add(x, y))
