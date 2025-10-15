0b3cd5eb7bbd069571b59ac40b422fbb91f292c8
Salahs03 <salahsiam03@gmail.com>
2025-10-15 13:42:23 +0100

    Add coffee machine class demonstrating abstraction

--- control_flow/coffee_machine.py
+++ control_flow/coffee_machine.py
@@ -1,8 +1,8 @@
 class CoffeeMachine:
     def start(self):
-        self._heat_water()
-        self._brew_coffee()
-        print("Coffee is ready!")
+        self._heat_water()   # heat the water
+        self._brew_coffee()  # brew the coffee
+        print("Coffee is ready!")  # final message
 
     def _heat_water(self):
         print("Heating water...")
@@ -10,5 +10,7 @@ class CoffeeMachine:
     def _brew_coffee(self):
         print("Brewing coffee...")
 
+# create a CoffeeMachine object and start it
 machine = CoffeeMachine()
 machine.start()
+
