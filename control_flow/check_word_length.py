66d52c4d5b3972b26db1fd5f23c590d22ed724a4
Salahs03 <salahsiam03@gmail.com>
2025-11-05 17:13:35 +0000

    Add function example checking word length

---
+++ control_flow/check_word_length.py
@@ -0,0 +1,9 @@
+def describe_word(word):
+    if len(word) > 5:
+        print(f"'{word}' is a long word.")
+    else:
+        print(f"'{word}' is a short word.")
+
+describe_word("python")
+
+# This code checks if a word is long or short
