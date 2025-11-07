07c8129e0d79106aaab8bcbc5abe80919c613204
Salahs03 <salahsiam03@gmail.com>
2025-11-07 16:04:07 +0000

    Add tuple example using a loop to print colors

---
+++ data_structure/tuple_loop.py
@@ -0,0 +1,9 @@
+colors = ("red", "green", "blue")
+
+def print_colors():
+    for color in colors:
+        print("Color:", color)
+
+print_colors()
+
+# This code loops through a tuple and prints each color
